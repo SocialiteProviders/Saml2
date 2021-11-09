SocialiteProviders/Saml2

commit 0e4edbd165fae33bbb77b0688c6e2241cee65116

Use RequestOptions constants. (#712)

Provider.php
@@ -2,6 +2,7 @@
 
 namespace SocialiteProviders\Saml2;
 
+use _HumbugBox58fd4d9e2a25\RdKafka\Metadata;
 use DateTime;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
@@ -31,7 +32,6 @@ use LightSaml\Model\Context\SerializationContext;
 use LightSaml\Model\Metadata\AssertionConsumerService;
 use LightSaml\Model\Metadata\EntityDescriptor;
 use LightSaml\Model\Metadata\KeyDescriptor;
-use LightSaml\Model\Metadata\Metadata;
 use LightSaml\Model\Metadata\SpSsoDescriptor;
 use LightSaml\Model\Protocol\AuthnRequest;
 use LightSaml\Model\Protocol\NameIDPolicy;

commit 1990cb5f5a95d1d99a778b2685979bac9bb21b39

Make SAML2 binding configurable & fix regression introduced by #712 (#767)
Co-authored-by: atymic <atymicq@gmail.com>

Provider.php
@@ -2,7 +2,6 @@
 
 namespace SocialiteProviders\Saml2;
 
-use _HumbugBox58fd4d9e2a25\RdKafka\Metadata;
 use DateTime;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
@@ -32,6 +31,7 @@ use LightSaml\Model\Context\SerializationContext;
 use LightSaml\Model\Metadata\AssertionConsumerService;
 use LightSaml\Model\Metadata\EntityDescriptor;
 use LightSaml\Model\Metadata\KeyDescriptor;
+use LightSaml\Model\Metadata\Metadata;
 use LightSaml\Model\Metadata\SpSsoDescriptor;
 use LightSaml\Model\Protocol\AuthnRequest;
 use LightSaml\Model\Protocol\NameIDPolicy;
@@ -93,6 +93,7 @@ class Provider extends AbstractProvider implements SocialiteProvider
             'certificate',
             'sp_acs',
             'sp_entityid',
+            'idp_binding_method',
         ];
     }
 
@@ -109,9 +110,11 @@ class Provider extends AbstractProvider implements SocialiteProvider
     {
         $this->request->session()->put('state', $state = $this->getState());
 
+        $binding = $this->getConfig('idp_binding_method', SamlConstants::BINDING_SAML2_HTTP_REDIRECT);
+
         $identityProviderConsumerService = $this->getIdentityProviderEntityDescriptor()
             ->getFirstIdpSsoDescriptor()
-            ->getFirstSingleSignOnService(SamlConstants::BINDING_SAML2_HTTP_REDIRECT);
+            ->getFirstSingleSignOnService($binding);
 
         $authnRequest = new AuthnRequest();
         $authnRequest